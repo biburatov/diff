187d0528035392a339f122fc6014d537caf8e3dc
biburatov <84617784+biburatov@users.noreply.github.com>
2021-05-22 16:42:06 +0700

    diff

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+    
+            {
+                Console.Write("Введите размер квадратной матрицы: ");
+                //Вводим и читаем размер
+                int r = Convert.ToInt16(Console.ReadLine());
+                //создаём
+                int[,] Matrix = new int[r, r];
+
+                //заполняем случайными числами от 0 до 100
+                Random random = new Random();
+                int rand;
+                for (int i = 0; i < r; i++)
+                {
+                    for (int j = 0; j < r; j++)
+                    {
+                        rand = random.Next(0, 100);
+                        Matrix[i, j] = rand;
+                    }
+                }
+                for (int i = 0; i < r; i++)
+                {
+                    for (int j = 0; j < r; j++)
+                    {
+                        Console.Write(Matrix[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
+    
